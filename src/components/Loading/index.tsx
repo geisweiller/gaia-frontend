f3d68b8d67c61f57f280ee85725b7f7f261c839a
Arthur Geisweiller <50842954+geisweiller@users.noreply.github.com>
2020-12-28 03:10:10 -0300

    loading

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,8 @@
+import './index.scss';
+import loading from '../../assets/svg/logo/loading.svg';
+
+export default function Loading() {
+  return (
+    <img  className='loading' src={loading} alt='Loading'/>
+  )
+}
