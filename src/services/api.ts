75ff0b4560c0adb655614112182ecd4c528240eb
Arthur Geisweiller <50842954+geisweiller@users.noreply.github.com>
2021-01-04 08:14:39 -0300

    cors policy bypass

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://cors-anywhere.herokuapp.com/http://homolog.grupogaia.com.br/prova/front/api/clients'
+  baseURL: 'http://homolog.grupogaia.com.br/prova/front/api/clients'
 });
 
 export default api;
+
+//https://cors-anywhere.herokuapp.com/
